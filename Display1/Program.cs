76156e777b3d887cd5a485b4a98525be2cbc3854
MTasreeq <tasreeq.adams@truenorthgroup.co.za>
2023-05-24 09:38:09 +0200

    Add project files.

---
+++ Display1/Program.cs
@@ -0,0 +1,35 @@
+using Display1.Data;
+using Display1.Models;
+using Display1.Service;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+//Search service
+builder.Services.AddScoped<SearchService>();
+builder.Services.AddScoped<AdventureWorks2019Context>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ Display1/Service/SearchService.cs
@@ -0,0 +1,73 @@
+﻿using Display1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Display1.Service
+{
+    public class SearchService
+    {
+        private AdventureWorks2019Context _db;
+
+        public string? SearchInput { get; set; }
+        public Person? SelectedPerson { get; set; }
+        public List<Person> SearchResults { get; set; }
+        public EmployeeDepartmentHistory? SelectedPersonHistory { get; set; }
+        public EmployeePayHistory? SelectedEmployeePayHistory { get; set; }
+        public Display1.Models.Address? SelectedAddress { get; set; }
+
+        public event Action<Person>? OnUserSelected;
+
+        public SearchService(AdventureWorks2019Context dbContext)
+        {
+            _db = dbContext;
+            SearchResults = new List<Person>();
+        }
+
+        public void PerformSearch()
+        {
+            if (!string.IsNullOrEmpty(SearchInput))
+            {
+                SearchResults = _db.Person
+                    .AsEnumerable()
+                    .Where(p => p.PersonType == "EM" &&
+                                (p.FirstName.Contains(SearchInput, StringComparison.OrdinalIgnoreCase) ||
+                                p.LastName.Contains(SearchInput, StringComparison.OrdinalIgnoreCase)))
+                    .ToList();
+            }
+            else
+            {
+                SearchResults.Clear();
+            }
+        }
+
+        public void SelectUser(Person person)
+        {
+            if (person.PersonType == "EM")
+            {
+                SelectedPerson = person;
+                SelectedPersonHistory = GetEmployeeDepartmentHistory(person.BusinessEntityId);
+                OnUserSelected?.Invoke(person);
+            }
+        }
+
+        public EmployeeDepartmentHistory? GetEmployeeDepartmentHistory(int businessEntityId)
+        {
+            return _db.EmployeeDepartmentHistory
+                .FirstOrDefault(edh => edh.BusinessEntityId == businessEntityId);
+        }
+
+        public EmployeePayHistory? GetEmployeePayHistory(int businessEntityId)
+        {
+            return _db.EmployeePayHistory.FirstOrDefault(p => p.BusinessEntityId == businessEntityId);
+        }
+
+        public Display1.Models.Address? GetAddressForBusinessEntity(int businessEntityId)
+        {
+            return _db.BusinessEntityAddress
+                .Include(bea => bea.Address)
+                .FirstOrDefault(bea => bea.BusinessEntityId == businessEntityId)?.Address;
+        }
+    }
+}
