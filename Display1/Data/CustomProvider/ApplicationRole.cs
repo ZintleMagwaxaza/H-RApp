439fa2c8bccbd222f4d6043931b6537c5750c705
ZintleMagwaxaza <132381183+ZintleMagwaxaza@users.noreply.github.com>
2023-06-02 17:30:32 +0200

    Controller/Models/Custom Added

---
+++ Display1/Data/CustomProvider/ApplicationRole.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Display1.Data.CustomProvider
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
+
+
+
+
+
+
---
+++ Display1/Data/CustomProvider/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Display1.Data.CustomProvider
+{
+    public class ApplicationUser : IdentityUser
+    {
+      
+        public bool AuthenticationType { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public string Name { get; set; }
+        public string PasswordSalt { get; internal set; }
+        public int BusinessEntityId { get; internal set; }
+        public string Email { get; internal set; }
+
+        [NotMapped]
+        public List<string> Roles { get; set; }
+        public bool IsAdmin { get; internal set; }
+        // public object Roles { get; internal set; }
+    }
+}
+
